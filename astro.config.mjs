c593b87f4861b0ee109f3a8686cc06fb9e695743
Ye Wang <yewang@redhat.com>
2024-06-14 10:42:46 +0800

    fix: remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
-import react from '@astrojs/react';
-import astroI18next from 'astro-i18next';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+import react from "@astrojs/react";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/security/compliances',
+  // base: '/security/compliances',
   // server: { host: "prod.foo.redhat.com", port: 8080, open: true },
-  output: 'server',
+  output: "server",
   adapter: node({
-    mode: 'standalone',
+    mode: "standalone",
   }),
   integrations: [tailwind(), react(), astroI18next()],
 });
